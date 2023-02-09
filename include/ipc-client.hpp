beb07de12fe4da82127d9af639aea3a9e97d4904
unknown <stevensoftware52@gmail.com>
2023-02-09 14:27:18 -0500

    Fix clang

--- include/ipc-client.hpp
+++ include/ipc-client.hpp
@@ -54,7 +54,7 @@ public:
 			  int64_t &cbid = g_cbid) = 0;
 
 	virtual std::vector<ipc::value> call_synchronous_helper(const std::string &cname, const std::string &fname, const std::vector<ipc::value> &args) = 0;
-	
+
 	std::atomic_bool m_shutting_down = false;
 	call_on_freez_t freez_cb = nullptr;
 	std::string app_state_path;
